a5470bc425cc2ac328e7395d647e0495e0b0e3c3
ntluongdev95 <ntluongsutra62@gmail.com>
2024-10-08 23:11:40 +0800

    add tx_test.go

--- custom/gov/types/keys.go
+++ custom/gov/types/keys.go
@@ -2,6 +2,18 @@ package types
 
 import "encoding/binary"
 
+const (
+	// ModuleName is the name of the module
+	ModuleName = "gov"
+
+	// StoreKey is the store key string for gov
+	StoreKey = ModuleName
+
+	// RouterKey is the message route for gov
+	RouterKey = ModuleName
+)
+
+
 // Keys for governance store
 // Items are stored with the following key: values
 var (
